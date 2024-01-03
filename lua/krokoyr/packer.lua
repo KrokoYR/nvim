d9df68bac56363b7037a376a64119e8b58303adf
Lopson Balzhinimaev <lopson@superdao.co>
2024-01-03 23:40:43 +0300

    chore: added tagbar

--- lua/krokoyr/packer.lua
+++ lua/krokoyr/packer.lua
@@ -47,6 +47,7 @@ return require('packer').startup(function(use)
   use("nvim-treesitter/playground")
   use("theprimeagen/harpoon")
   use("theprimeagen/refactoring.nvim")
+  use("preservim/tagbar")
   use("mbbill/undotree")
   use("tpope/vim-fugitive")
   use("nvim-treesitter/nvim-treesitter-context");
