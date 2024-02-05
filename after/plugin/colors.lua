bc23366b43dfda6a5e025afc72423dc56b9de5f5
Lopson Balzhinimaev <geekwebs@yandex.com>
2024-02-06 00:47:25 +0300

    chore: updated color scheme to tokyonight

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,13 +1,29 @@
-require('rose-pine').setup({
-    disable_background = true
+require("tokyonight").setup({
+    -- use the night style
+    style = "night",
+    -- disable italic for functions
+    transparent = true,
+    terminal_colors = false,
+    styles = {
+        functions = {},
+        sidebars = "transparent",
+        floats = "transparent"
+    },
+    sidebars = { "qf" },
 })
 
 function ColorMyPencils(color)
-    color = color or "rose-pine"
-    vim.cmd.colorscheme(color)
+    color = color or "tokyonight"
+    vim.cmd("colorscheme " .. color)
 
+    -- Setting background to 'none' for Normal and NormalFloat highlight groups
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+    vim.api.nvim_set_hl(0, "CursorLineNr", { bg = "none" })
+    vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+    vim.opt.fillchars = { eob = " " }
 end
 
+-- Apply the Tokyo Night color scheme
 ColorMyPencils()
--- lua/krokoyr/packer.lua
+++ lua/krokoyr/packer.lua
@@ -19,10 +19,9 @@ return require('packer').startup(function(use)
     }
 
     use({
-        'rose-pine/neovim',
-        as = 'rose-pine',
+        'folke/tokyonight.nvim',
         config = function()
-            vim.cmd('colorscheme rose-pine')
+            vim.cmd [[colorscheme tokyonight]]
         end
     })
 
