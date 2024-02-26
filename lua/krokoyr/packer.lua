c0270059f6cee5e656673eba459c5f30422666be
Lopson Balzhinimaev <geekwebs@yandex.com>
2024-02-26 10:04:34 +0100

    chore: added metals

--- lua/krokoyr/packer.lua
+++ lua/krokoyr/packer.lua
@@ -12,6 +12,13 @@ return require('packer').startup(function(use)
         -- or                            , branch = '0.1.x',
         requires = { { 'nvim-lua/plenary.nvim' } }
     }
+    use {
+        'scalameta/nvim-metals',
+        requires = {
+            'nvim-lua/plenary.nvim',
+            'mfussenegger/nvim-dap',
+        },
+    }
 
     use {
         'nvim-telescope/telescope-fzf-native.nvim',
