9abd7d63c32ac73f412eae64dafd349bf44dd5c4
KrokoYR <geekwebs@yandex.com>
2024-04-07 12:22:08 +0200

    chore: added keymap for tagbar

--- lua/krokoyr/remap.lua
+++ lua/krokoyr/remap.lua
@@ -57,3 +57,4 @@ vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
+vim.keymap.set("n", "<leader>tg", ":TagbarToggle<CR>")
